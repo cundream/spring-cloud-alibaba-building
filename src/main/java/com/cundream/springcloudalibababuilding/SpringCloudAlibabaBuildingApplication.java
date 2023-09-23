69824916508d3bebef022dfaf6d54318514c7295
李村 <cundream@163.com>
2023-09-23 09:24:33 +0800

    Initial commit

---
+++ src/main/java/com/cundream/springcloudalibababuilding/SpringCloudAlibabaBuildingApplication.java
@@ -0,0 +1,13 @@
+package com.cundream.springcloudalibababuilding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaBuildingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaBuildingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cundream/springcloudalibababuilding/SpringCloudAlibabaBuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cundream.springcloudalibababuilding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaBuildingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
